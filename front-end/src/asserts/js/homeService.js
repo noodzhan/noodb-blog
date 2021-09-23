ba2c5801190e6f881bdb1aa1d880f5b1bb34854b
占志虎 <noodzhan@163.com>
2021-09-23 23:34:56 +0800

    feature(projectManage): 添加项目管理md

---
+++ front-end/src/asserts/js/homeService.js
@@ -0,0 +1,10 @@
+/**
+ * 所有Home.vue发出的请求业务
+ */
+class HomeService {
+  getAllArticleSummary ($vm) {
+    $vm.$http({
+      url: ''
+    })
+  }
+}
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -6,9 +6,11 @@ import 'ant-design-vue/dist/antd.css'
 import '../public/assert/style/public.css'
 import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
+import axios from 'axios'
 Vue.config.productionTip = false
 Vue.use(Antd)
 Vue.use(mavonEditor)
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App)
