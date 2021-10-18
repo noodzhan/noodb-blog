8cf5bf569df06cec8019cfe3a3edf66976cb7a4b
占志虎 <noodzhan@163.com>
2021-10-18 23:52:33 +0800

    feature(登录): 添加必填提示

--- front-end/src/views/Login.vue
+++ front-end/src/views/Login.vue
@@ -1,20 +1,19 @@
 <template>
   <div style="margin: 20vh auto;width: 300px;display: flex;flex-direction: column;">
     <img src="@/asserts/img/logo.png" style="margin: 15px auto">
-    <a-form-model v-model="formObj" :label-col="{span:4}" :wrapper-col="{span: 20}"
-    >
-      <a-form-item label="用户名">
+    <a-form-model :model="formObj" :label-col="{span: 8}" :wrapper-col="{span: 16}" ref="loginForm">
+      <a-form-item label="用户名" prop="username" :required="true">
         <a-input v-model="formObj.username">
           <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)"/>
         </a-input>
       </a-form-item>
-      <a-form-item label="密码">
-        <a-input v-model="formObj.password" type="password">
+      <a-form-item label="密码" prop="password" :required="true">
+        <a-input v-model="formObj.password" type="password" @keypress.enter="onSubmit">
           <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)"/>
         </a-input>
       </a-form-item>
       <a-form-item class="login-footer" :wrapper-col="{span:24}">
-        <a-button>
+        <a-button @click="resetForm">
           重置
         </a-button>
         <a-button type="primary" @click="onSubmit">
@@ -31,30 +30,40 @@ export default {
   components: {},
   data: function () {
     return {
-      formObj: {}
-    }
-  },
-  computed: {
-    visible: function () {
-      return this.$store.state.isLogin
+      formObj: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [{ required: true, message: '用户名不能为空', trigger: 'blur' }],
+        password: [{ required: true, message: '用户名不能为空', trigger: 'blur' }]
+      }
     }
   },
   methods: {
     onSubmit () {
-      this.$http({
-        url: this.$appUrl + this.autoPrefix() + '/user/login',
-        method: 'post',
-        data: this.formObj
-      }).then(resp => {
-        if (resp.data.code === 0) {
-          // 成功
-          this.$store.commit('setLoginFlag', true)
-          this.$store.commit('setUserInfo', resp.data.data)
-          this.$router.push('/home')
-        } else {
-          this.$notification.error({ message: '登入失败' })
+      const $vm = this
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          $vm.$http({
+            url: $vm.$appUrl + this.autoPrefix() + '/user/login',
+            method: 'post',
+            data: $vm.formObj
+          }).then(resp => {
+            if (resp.data.code === 0) {
+              // 成功
+              $vm.$store.commit('setLoginFlag', true)
+              $vm.$store.commit('setUserInfo', resp.data.data)
+              $vm.$router.push('/home')
+            } else {
+              $vm.$notification.error({ message: '登入失败' })
+            }
+          })
         }
       })
+    },
+    resetForm () {
+      this.$refs.loginForm.resetFields()
     }
 
   }
