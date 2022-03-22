f9fd7edff3c661dce0e69855c53bc5f34ec005c8
noodzhan <noodzhan@163.com>
2022-03-22 19:15:17 +0800

    fix:  home page title bug

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -3,7 +3,8 @@ module.exports = {
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:8095/api',
+        // target: 'http://localhost:8095/api',
+        target: 'https://noodb.com/api',
         changeOrigin: true,
         ws: true,
         pathRewrite: {
@@ -20,11 +21,11 @@ module.exports = {
    * 修改插件的配置
    * @param config
    */
-  // chainWebpack: config => {
-  //   config.plugin('html').tap(arg => {
-  //     arg[0].title = 'noodb'
-  //     return arg
-  //   })
-  // },
+  chainWebpack: config => {
+    config.plugin('html').tap(arg => {
+      arg[0].title = 'noodb个人博客'
+      return arg
+    })
+  },
   productionSourceMap: false
 };
