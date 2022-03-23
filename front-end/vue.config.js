e7e25be084954b02ed97a63bc127679650f9eaa8
noodzhan <noodzhan@163.com>
2022-03-23 16:45:26 +0800

    refactor prettier formatter

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -3,7 +3,8 @@ module.exports = {
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:8095/api',
+        // target: 'http://localhost:8095/api',
+        target: 'https://noodb.com/api',
         changeOrigin: true,
         ws: true,
         pathRewrite: {
@@ -16,12 +17,13 @@ module.exports = {
   configureWebpack: {
     devtool: 'source-map'
   },
+
   /**
-     * 修改插件的配置
-     * @param config
-     */
-  chainWebpack: config => {
-    config.plugin('html').tap(arg => {
+   * 修改插件的配置
+   * @param config
+   */
+  chainWebpack: (config) => {
+    config.plugin('html').tap((arg) => {
       arg[0].title = 'noodb个人博客';
       return arg;
     });
