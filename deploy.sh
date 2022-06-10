808de5acdb14f60f16818a6bfc6407272f615b02
占志虎 <noodzhan@163.com>
2022-06-10 23:01:33 +0800

    feat: 实现自动部署nuxtapp

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-# echo 'ssh 1.15.231.74'
-# ssh ubuntu@1.15.231.74 <./etc/backup.sh
-# echo 'scp'
-# cd nuxt
-# npm run build
-# pwd
-# scp -r ./.nuxt nuxt.config.js package.json package-lock.json ./static ubuntu@1.15.231.74:/home/ubuntu/nblog/nuxtapp
+echo 'ssh 1.15.231.74'
+ssh ubuntu@1.15.231.74 <./etc/backup.sh
+echo 'scp'
+cd nuxtapp
+
+npm run build
+pwd
+scp -r ./.nuxt nuxt.config.js package.json package-lock.json ./static ubuntu@1.15.231.74:/home/ubuntu/nblog/nuxtapp
+
+# shellcheck disable=SC2103
+cd ..
 
 ssh ubuntu@1.15.231.74 <./etc/startNuxtapp.sh
