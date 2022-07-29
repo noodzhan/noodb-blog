8061fb7818a4cd4ed73f2dcb2ad6ec22a0e98c17
noodzhan <noodzhan@163.com>
2022-07-29 19:44:28 +0800

    fix: 临时提交

---
+++ tool/src/leetcode.js
@@ -0,0 +1,9 @@
+//1. 获取dom和样式
+//获取题目的信息。
+document.querySelector(".notranslate .notranslate").outerHTML;
+
+//复制样式
+document.querySelector("body>div").style = "";
+
+//2.获取代码,拦截这个请求，获取代码
+//https://leetcode.cn/problems/gray-code/submit/
